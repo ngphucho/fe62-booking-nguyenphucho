de45aec35240d0938269ea2aa7e3fadcf0710682
NguyenPhucHo <ngphucho@gmail.com>
2021-07-02 23:01:53 +0700

    fix header menu

--- src/components/ListCinemas/index.js
+++ src/components/ListCinemas/index.js
@@ -3,8 +3,8 @@ import CinemaItem from "../CinemaItem";
 
 export default function ListCinemas({ danhSachHeThongRap }) {
   return (
-    <div className="container-fluid">
-      <div className="row listCinema">
+    <div className="container-fluid px-0">
+      <div className="row listCinema mx-0">
         {danhSachHeThongRap.map((item, index) => (
           <div className="col-md-2 col-auto cinemaItem" key={index}>
             <CinemaItem cinema={item} />
